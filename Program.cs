3b9abf4f3640075eeb37b533ad79e2b19f705cab
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-11-01 13:45:54 +0000

    Init DotNet Core console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace student_grade_profile_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
