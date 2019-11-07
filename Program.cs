9f7e5ce32d0b51b220ae3e6770d7d368fde9b0ad
Jamie Hamilton <jamiehamilton81@gmail.com>
2019-11-07 23:01:56 +0000

    Imported generic collections and instantiated a list to hold students

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace student_grade_profile_application
 {
@@ -8,6 +9,12 @@ namespace student_grade_profile_application
         {
             Student student1 = new Student("John","Williams","Computing and Information Systems", "Male", "22082000", "1804492");
             student1.PrintData();
+
+            Student student2 = new Student("Sarah", "Smith", "Computing and Information Systems", "Female", "06121999", "1807794");
+            student2.PrintData();
+
+            List <Student> students = new List<Student>();
+
         }
 
     }
