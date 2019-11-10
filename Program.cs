1ef9fdb5cc8d668ccb3ab18151c5d752fe68674f
Jamie Hamilton <jamiehamilton81@gmail.com>
2019-11-10 20:06:24 +0000

    Create list of students + added students. Foreach loop added

--- Program.cs
+++ Program.cs
@@ -7,13 +7,23 @@ namespace student_grade_profile_application
     {
         static void Main(string[] args)
         {
+
+
             Student student1 = new Student("John","Williams","Computing and Information Systems", "Male", "22082000", "1804492");
-            student1.PrintData();
 
             Student student2 = new Student("Sarah", "Smith", "Computing and Information Systems", "Female", "06121999", "1807794");
-            student2.PrintData();
 
             List <Student> students = new List<Student>();
+
+            students.Add(student1);
+            students.Add(student2);
+
+            foreach(Student student in students)
+            {
+              student.PrintData();
+            }
+
+
 
         }
 
--- Student.cs
+++ Student.cs
@@ -4,12 +4,13 @@ namespace student_grade_profile_application
 {
     class Student
     {
-            string firstName;
-            string lastName;
+            protected string firstName;
+            protected string lastName;
             string course;
             string gender;
             string dateOfBirth;
             string studentID;
+
         public Student(string firstName, string lastName, string course, string gender, string dateOfBirth, string studentID)
         {
             this.firstName = firstName;
