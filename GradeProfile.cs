24fbffed2fac1fbd44238b443a2b80fb4341d53f
Jamie Hamilton <jamiehamilton81@gmail.com>
2019-11-14 22:17:06 +0000

    Addressed average method

--- GradeProfile.cs
+++ GradeProfile.cs
@@ -1,17 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace student_grade_profile_application
 {
     class GradeProfile
     {
-        /*public decimal Average(int year, )
+        public decimal Average(string studentID, int year, List<Grade> gradeProfile)
         {
             decimal avg;
 
-            foreach (Grade grade in grades)
+            foreach (Grade grade in gradeProfile.Where(grade => grade.yearOfStudy == year && grade.studentID == studentID))
             {
-
+                
             }
-        }*/
+        }
     }
 }
